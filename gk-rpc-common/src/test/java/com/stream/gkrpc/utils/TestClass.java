6cb2c085022d2acd910c39c9ccd6d75991f9b414
stream1080 <fmxhax638@foxmail.com>
2021-11-06 16:53:14 +0800

    add: 开发公共模块common

---
+++ gk-rpc-common/src/test/java/com/stream/gkrpc/utils/TestClass.java
@@ -0,0 +1,19 @@
+package com.stream.gkrpc.utils;
+
+/**
+ * 封装的测试对象
+ *
+ * @author stream
+ * @since 2021/11/6 16:51
+ */
+public class TestClass {
+    private String a(){
+        return "a";
+    }
+    public String b(){
+        return "b";
+    }
+    protected String c(){
+        return "c";
+    }
+}
